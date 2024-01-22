bea30f6a58c6b073cea29ad6ebbbefb9476c31de
TUSHAR PANJA <mailtusharpanja@gmail.com>
2024-01-22 15:34:40 +0530

    Update UserInterface.java

--- Stage 1/Java Programming Fundamentals/Java 11-12/Holiday Homework - String Methods (Java 11)/UserInterface.java
+++ Stage 1/Java Programming Fundamentals/Java 11-12/Holiday Homework - String Methods (Java 11)/UserInterface.java
@@ -1,17 +1,17 @@
 import java.util.stream.Stream;
 //import org.apache.commons.lang3.StringUtils;
 
-public class User Interface {
+public class UserInterface {
 	public static void main(String args[]) {
 		String str1="      Hello \n       Welcome to\n      New String Methods in Java   ";
 		String str2="Programming";
 		int n=5;
-		boolean res-validateSubmission (str1);
+		boolean res = validateSubmission (str1);
 		if (res==true)
 			System.out.println("The String is empty");
 		else
 			System.out.println("The String is not empty");
-		Stream<String> res1-extractLines (str1);
+		Stream<String> res1= extractLines (str1);
 		System.out.println("The extracted lines from the String are");
 		res1.forEach(s->System.out.println(s.trim()));
 		String res2=removeWhiteSpaces (str1);
@@ -24,14 +24,7 @@ public class User Interface {
 	// Use isBlank () method here
 	public static boolean validateSubmission (String str)
 	{
-		// Fill the ClassLoader
-		boolean bool = str.isBlank ();
-		if (bool) {
-			return true;
-		}
-		else{
-			return false;
-		}
+		return str.isBlank();
 	}
 	
 	// Use lines () method here
@@ -55,7 +48,7 @@ public class User Interface {
 	public static String checkSubmission (String str,int n)
 	{
 		// Fill the code
-		String str2-str.replaceAll("[aeiouAEIOU]","");
+		String str2 = str.replaceAll("[aeiouAEIOU]","");
 		return (str2.repeat (n));
 		//return null;
 	}
