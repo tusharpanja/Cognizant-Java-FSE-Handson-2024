7747ee42d5d203be19e6cb6d49fc0a71e413ef6d
TUSHAR PANJA <mailtusharpanja@gmail.com>
2024-01-22 15:32:41 +0530

    Update UserInterface.java

--- Stage 1/Java Programming Fundamentals/Java 11-12/EB Connection - Switch Enhancements & Compact Number Formatting/UserInterface.java
+++ Stage 1/Java Programming Fundamentals/Java 11-12/EB Connection - Switch Enhancements & Compact Number Formatting/UserInterface.java
@@ -1,65 +1,113 @@
-﻿import java.util.Scanner; 
+import java.util.Scanner;
 import java.util.Locale;
-import java.text.NumberFormat;
+import java.text.NumberFormat ;
 
-public class UserInterface {
-	public static void main(String args[]) {
-		Scanner sc=new Scanner(System.in);
-		System.out.println("Enter the Connection ID :");
-		int connectionId=sc.nextInt();
-		System.out.println("Enter the Connection Type :" );
-		String connectionType=sc.next();
-		System.out.println("Enter the Previous Reading :" );
-		int previousReading=sc.nextInt();
-		System.out.println( "Enter the Current Reading :" );
-		int currentReading=sc.nextInt();
-		int readingValue = currentReading-previousReading; 
-		outer: {
-			String connection=checkConnectionType (connectionType);
-			System.out.println(connection);
-			if(connection.equals("Invalid Connection"))
-			{
-				break outer;
-			}
-			int billAmount;
-			if(readingValue>0 && readingValue<=200)
-			{
-				billAmount readingValue*2;
-			}
-			else
-			{
-				billAmount = readingValue*5;
-			}
-			System.out.println("Bill Amount (LongNumberFormat) : "+longNumberFormat (billAmount)); 
-			System.out.println("Bill Amount (Short NumberFormat) : "+shortNumberFormat(billAmount));
-		}
-	}
-	// Use Enhanced switch statement to check the connection type
-	public static String checkConnectionType(String connectionType) {
-	//Fill the Code
-		switch(connectionType) {
-			case "Single Phase", "Three Phase", "SINGLEPHASE", "THREEPHASE" : 
-				return ("Valid Connection");
-				//break;
-			default:
-				return ("Invalid Connection");
-		}
-	}
-  // Code by, Abhishek Sharma
-	// Use Compact Number Formatting to convert the number into Long Number Format
-	finden into Long Number
-	public static String longNumberFormat(int number) {
-	//Fill the Code
-		NumberFormat formatter = NumberFormat.getCompact NumberInstance( Locale.US, NumberFormat.Style. LONG);
-		String res = formatter.format(number);
-		return res;
-	}
 
-	// Use compact number formatting to convert the number into  short number format
-	public static String shortNumberFormat(int number){
-		//Fill the code
-		NumberFormat formatter = NumberFormat.getCompactNumberInstance(Locale.US, NumberFormat.Style.SHORT);
-		String res1 = formatter.format(number);
-		return res1;
-	}
+class UserInterface {
+   public static void main(String[] args) {
+       Scanner scanner = new Scanner(System.in);
+
+       System.out.println("Enter the Connection ID : ");
+       int connectionId = scanner.nextInt();
+
+       System.out.println("Enter the Connection Type : ");
+       String connectionType = scanner.next().toUpperCase();
+
+       System.out.println("Enter the Previous Reading : ");
+       int previousReading = scanner.nextInt();
+
+       System.out.println("Enter the Current Reading : ");
+       int currentReading = scanner.nextInt();
+
+       String connectionStatus = checkConnectionType(connectionType);
+       if (connectionStatus.equals("Valid Connection")) {
+           int readingValue = currentReading - previousReading;
+           double price = readingValue > 200 ? 5 : 2;
+           double billAmount = readingValue * price;
+
+           System.out.println("Bill Amount (LongNumberFormat) : " + longNumberFormat((int)billAmount));
+           System.out.println("Bill Amount (ShortNumberFormat) : " + shortNumberFormat((int)billAmount));
+       } else {
+           System.out.println("Invalid Connection");
+       }
+   }
+
+   public static String checkConnectionType(String connectionType) {
+       switch(connectionType){
+        case "SinglePhase" , "ThreePhase" , "SINGLEPHASE" , "THREEPHASE" :
+               return ("Valid Connection");
+        default: 
+                return ("Invalid Connection");
+       }
+   }
+
+   public static String longNumberFormat(int number) {
+    NumberFormat  formatter = NumberFormat.getCompactNumberInstance(Locale.US, NumberFormat.Style. LONG);
+    String res = formatter.format(number);
+    return res ;
+}
+
+
+   public static String shortNumberFormat(int number) {
+        NumberFormat formatter = NumberFormat.getCompactNumberInstance(Locale.US , NumberFormat.Style.SHORT);
+        String res1 = formatter.format(number) ;
+        return res1 ;
+   }
+
+}import java.util.Scanner;
+import java.util.Locale;
+import java.text.NumberFormat ;
+
+
+class UserInterface {
+   public static void main(String[] args) {
+       Scanner scanner = new Scanner(System.in);
+
+       System.out.println("Enter the Connection ID : ");
+       int connectionId = scanner.nextInt();
+
+       System.out.println("Enter the Connection Type : ");
+       String connectionType = scanner.next().toUpperCase();
+
+       System.out.println("Enter the Previous Reading : ");
+       int previousReading = scanner.nextInt();
+
+       System.out.println("Enter the Current Reading : ");
+       int currentReading = scanner.nextInt();
+
+       String connectionStatus = checkConnectionType(connectionType);
+       if (connectionStatus.equals("Valid Connection")) {
+           int readingValue = currentReading - previousReading;
+           double price = readingValue > 200 ? 5 : 2;
+           double billAmount = readingValue * price;
+
+           System.out.println("Bill Amount (LongNumberFormat) : " + longNumberFormat((int)billAmount));
+           System.out.println("Bill Amount (ShortNumberFormat) : " + shortNumberFormat((int)billAmount));
+       } else {
+           System.out.println("Invalid Connection");
+       }
+   }
+
+   public static String checkConnectionType(String connectionType) {
+       switch(connectionType){
+        case "SinglePhase" , "ThreePhase" , "SINGLEPHASE" , "THREEPHASE" :
+               return ("Valid Connection");
+        default: 
+                return ("Invalid Connection");
+       }
+   }
+
+   public static String longNumberFormat(int number) {
+    NumberFormat  formatter = NumberFormat.getCompactNumberInstance(Locale.US, NumberFormat.Style. LONG);
+    String res = formatter.format(number);
+    return res ;
+}
+
+
+   public static String shortNumberFormat(int number) {
+        NumberFormat formatter = NumberFormat.getCompactNumberInstance(Locale.US , NumberFormat.Style.SHORT);
+        String res1 = formatter.format(number) ;
+        return res1 ;
+   }
+
 }
